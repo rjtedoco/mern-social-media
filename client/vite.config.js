4d7f5437a9185383425bc0608f96ada7ebfa0a53
RJ <rjay.tedoco@gmail.com>
2023-05-25 23:32:43 +0800

    Add user profile widget

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+/** @type {import('vite').UserConfig} */
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
+
+  resolve: {
+    alias: {
+      components: "/src/components",
+    },
+  },
+});
