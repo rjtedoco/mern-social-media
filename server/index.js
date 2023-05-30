025ff3d65bd05254ec969099079380fcbff70498
RJ <rjay.tedoco@gmail.com>
2023-05-30 08:10:23 +0800

    Fix post route

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ const upload = multer({ storage });
 
 // Routes with Files
 app.post("/auth/register", upload.single("picture"), register);
-app.post("/posts/", verifyToken, upload.single("picture"), createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 // Routes
 app.use("/auth", authRoutes);
